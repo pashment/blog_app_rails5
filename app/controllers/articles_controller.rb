bb4e6a3cc7f7c615ed90224f39c08d18920e0b2a
petra <pashment@gmail.com>
2019-02-28 22:06:32 +0000

    flash.now

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,13 +12,13 @@ if @article.save
 flash[:success] = "Article has been created"
 redirect_to articles_path 
 else
-flash[:danger] = "Article has not been created"
+flash.now[:danger] = "Article has not been created"
 render :new
 end
 end
 
 private
 def article_params
-params.require(:article).permit(:title, :body) 
-end
- end
+        params.require(:article).permit(:title, :body) 
+        end
+    end
